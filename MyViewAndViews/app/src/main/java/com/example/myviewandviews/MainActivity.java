cf7cf17146a0e2f4403f6aec8a6efa908c83e9b9
unknown <akur1@yahoo.com>
2020-10-28 22:56:53 +0700

    modul5

---
+++ MyViewAndViews/app/src/main/java/com/example/myviewandviews/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myviewandviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Google Pixel");
+    }
+}
